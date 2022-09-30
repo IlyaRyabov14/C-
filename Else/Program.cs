f07e2d9702f08e336d8d6f33bf357e93b482fdc7
Ryaba1488 <ilya-ryabov03@yandex.ru>
2022-09-30 16:36:08 +0300

    else

---
+++ Else/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите имя пользователя ");
+
+string username = Console.ReadLine();
+
+if(username == "Masha")
+{
+    Console.WriteLine("Eeeep, it's you");
+}
+else
+{
+   Console.Write("Привет ");
+   Console.WriteLine(username);
+}
+
+ 
+  
+
